b8fb0fc910baedbd514d0d9d6dfe3e0ce81ad181
alfito69420 <alfredo.aramburo240@gmail.com>
2024-11-14 23:49:25 -0600

    add: carga de datos y visualizacion del RUL

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 #   Librerías 
-import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -8,39 +7,60 @@ import seaborn as sns
 plt.style.use('bmh')
 sns.set_style("whitegrid")
 
+import os 
+os.environ['TCL_LIBRARY'] = 'C:/Users/Alfres/AppData/Local/Programs/Python/Python313/tcl/tcl8.6'
+os.environ['TK_LIBRARY'] = 'C:/Users/Alfres/AppData/Local/Programs/Python/Python313/tcl/tk8.6'
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+pd.set_option("display.max_rows", None)
+
+# se define el nombre de las columnas
+index_names = ['motor', 'ciclos_operacion']
+setting_names = ['setting_1', 'setting_2', 'setting_3']
+sensor_names = ['s_{}'.format(i) for i in range(1,22)] 
+col_names = index_names + setting_names + sensor_names
+
+#se cargan los datos
+train = pd.read_csv('CMAPSSData\\train_FD001.txt',sep='\\s+', header=None, names=col_names)
+test = pd.read_csv('CMAPSSData\\train_FD001.txt',sep='\\s+', header=None, names=col_names)
+y_test = pd.read_csv('CMAPSSData\\RUL_FD001.txt', sep='\\s+', header=None, names=['RUL'])
+
+# El archivo train contiene todas las caracteristicas como numero de unidad, parametros de configuracion y sensores
+# El archivo test contiene todas las caracteristicas como numero de unidad, parametros de configuracion y sensores
+# El archivo Y_test contiene el RUL para los datos de test
+train.head()
+print(train.head())
 
 
+train['motor'].unique() # Hay 100 motores no unicos
+y_test.shape    # RUL value for 100 no of engines 
+(100,1)
+print(train.describe())
 
-column_names = ['engine', 'time', 'op_setting_1', 'op_setting_2', 
-                'op_setting_3'] + [f'sm_{i}' for i in range(1, 22)]
+train=train.drop('setting_3',axis=1)
 
+def add_remaining_useful_life(df):
+    #   Obten el numero total de ciclos por cada unidad
+    grouped_by_unit = df.groupby(by="motor")
+    max_cycle = grouped_by_unit["ciclos_operacion"].max()
+    
+    #   Fusionar el ciclo máximo nuevamente en el marco original
+    result_frame = df.merge(max_cycle.to_frame(name='max_cycle'), left_on='motor', right_index=True)
+    
+    #   Calcula el RULL de cada fila
+    remaining_useful_life = result_frame["max_cycle"] - result_frame["ciclos_operacion"]
+    result_frame["RUL"] = remaining_useful_life
+    
+    #Se elimina max_cycle, ya no se ocupa
+    result_frame = result_frame.drop("max_cycle", axis=1)
+    return result_frame
 
-# assign names to columns , save in dict_list 
-Sensor_dictionary={}
-dict_list=[ "(Fan inlet temperature) (◦R)",
-"(LPC outlet temperature) (◦R)",
-"(HPC outlet temperature) (◦R)",
-"(LPT outlet temperature) (◦R)",
-"(Fan inlet Pressure) (psia)",
-"(bypass-duct pressure) (psia)",
-"(HPC outlet pressure) (psia)",
-"(Physical fan speed) (rpm)",
-"(Physical core speed) (rpm)",
-"(Engine pressure ratio(P50/P2)",
-"(HPC outlet Static pressure) (psia)",
-"(Ratio of fuel flow to Ps30) (pps/psia)",
-"(Corrected fan speed) (rpm)",
-"(Corrected core speed) (rpm)",
-"(Bypass Ratio) ",
-"(Burner fuel-air ratio)",
-"(Bleed Enthalpy)",
-"(Required fan speed)",
-"(Required fan conversion speed)",
-"(High-pressure turbines Cool air flow)",
-"(Low-pressure turbines Cool air flow)" ]
+train = add_remaining_useful_life(train)
+train[sensor_names+['RUL']].head()
 
-i=1
-for x in dict_list :
-    Sensor_dictionary[f'sm_{i}']=x
-    i+=1
-Sensor_dictionary
+df_max_rul = train[['motor', 'RUL']].groupby('motor').max().reset_index()
+df_max_rul['RUL'].hist(bins=15, figsize=(15,7))
+plt.xlabel('RUL')
+plt.ylabel('frequency')
+plt.show()
