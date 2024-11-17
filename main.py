9e66653e669e0d9c5f70afab783b7b30597560f8
alfito69420 <alfredo.aramburo240@gmail.com>
2024-11-17 14:37:09 -0600

    add: histograma y mapa de calor

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ pd.set_option("display.max_rows", None)
 # se define el nombre de las columnas
 index_names = ['motor', 'ciclos_operacion']
 setting_names = ['setting_1', 'setting_2', 'setting_3']
-sensor_names = ['s_{}'.format(i) for i in range(1,22)] 
+sensor_names = ['sensor_{}'.format(i) for i in range(1,22)] 
 col_names = index_names + setting_names + sensor_names
 
 #se cargan los datos
@@ -30,6 +30,10 @@ y_test = pd.read_csv('CMAPSSData\\RUL_FD001.txt', sep='\\s+', header=None, names
 # El archivo test contiene todas las caracteristicas como numero de unidad, parametros de configuracion y sensores
 # El archivo Y_test contiene el RUL para los datos de test
 train.head()
+
+train_head = train  # Usa el DataFrame completo
+train_head.to_csv('C:\\Users\\Alfres\\Desktop\\RULtrain_output.csv', sep='\t', index=False)
+
 print(train.head())
 
 
@@ -64,3 +68,44 @@ df_max_rul['RUL'].hist(bins=15, figsize=(15,7))
 plt.xlabel('RUL')
 plt.ylabel('frequency')
 plt.show()
+
+def plot_sensor(sensor_name):
+    plt.figure(figsize=(13,5))
+    for i in train['motor'].unique():
+        if (i % 10 == 0):  
+            plt.plot('RUL', sensor_name, 
+                    data=train[train['motor']==i])
+    plt.xlim(250, 0)  
+    plt.xticks(np.arange(0, 275, 25))
+    plt.ylabel(sensor_name)
+    plt.xlabel('Remaining Use fulLife')
+    plt.show()
+
+for sensor_name in sensor_names:
+    plot_sensor(sensor_name)
+    
+plt.figure(figsize=(25,18))
+sns.heatmap(train.corr(),annot=True ,cmap='Reds')
+plt.show()
+
+cor=train.corr()
+#Selecting highly correlated features
+train_relevant_features = cor[abs(cor['RUL'])>=0.5]
+train_relevant_features['RUL']
+
+list_relevant_features=train_relevant_features.index
+list_relevant_features=list_relevant_features[1:]
+list_relevant_features
+
+# Now we will keep onlt these imprtant features in both train & test dataset.
+train=train[list_relevant_features]
+
+# train & y_train
+# Calculated RUL variable is our Target variable.
+y_train=train['RUL']
+X_train=train.drop(['RUL'],axis=1)
+X_train.head(5)
+
+# Test data set , keeping only train columns/features.
+X_test=test[X_train.columns]
+X_test.head(5)
